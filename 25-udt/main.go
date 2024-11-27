a68427d80f7e3eac3462b9154809cb971864dcf2
Jiten <jpalaparthi@gmail.com>
2024-11-27 16:27:18 +0530

    add more examples

--- 25-udt/main.go
+++ 25-udt/main.go
@@ -17,6 +17,20 @@ func (mai myanotherint) Cube() int {
 	return int(mai * mai * mai)
 }
 
+type mybool bool
+
+func (mb mybool) Touint8() uint8 {
+	if mb {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func (mb mybool) ToString() string {
+	return fmt.Sprint(mb)
+}
+
 type integer = int // in all aspects both integer and int are same
 
 func main() {
@@ -39,5 +53,28 @@ func main() {
 	fmt.Println(c2)
 
 	var num3 myanotherint = 30
+	str3 := myint(num3).ToString()
+	s3 := myint(num3).Sq()
+	c3 := num3.Cube()
 
+	fmt.Println(str3)
+	fmt.Println(s3)
+	fmt.Println(c3)
+
+	var float1 float32 = 123.123
+
+	str4 := myint(float1).ToString()
+	s4 := myint(float1).Sq()
+	c4 := myanotherint(float1).Cube()
+
+	fmt.Println(str4)
+	fmt.Println(s4)
+	fmt.Println(c4)
+
+	var b1 mybool = true
+
+	str5 := myint(b1.Touint8()).ToString()
+	str6 := b1.ToString()
+	fmt.Println(str5)
+	fmt.Println(str6)
 }
