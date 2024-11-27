cff129f81a55c73632f3277be59c6daad01d30dc
Jiten <jpalaparthi@gmail.com>
2024-11-27 16:04:18 +0530

    add more examples

--- 25-udt/main.go
+++ 25-udt/main.go
@@ -1,5 +1,43 @@
 package main
 
+import "fmt"
+
+type myint int
+
+func (mi myint) ToString() string {
+	return fmt.Sprint(mi)
+}
+func (mi myint) Sq() int {
+	return int(mi * mi)
+}
+
+type myanotherint myint
+
+func (mai myanotherint) Cube() int {
+	return int(mai * mai * mai)
+}
+
+type integer = int // in all aspects both integer and int are same
+
 func main() {
+
+	var num1 myint = 100
+	str1 := num1.ToString()
+	s1 := num1.Sq()
+	c1 := myanotherint(num1).Cube()
+
+	fmt.Println(str1)
+	fmt.Println(s1)
+	fmt.Println(c1)
+
+	var num2 int = 25
+	str2 := myint(num2).ToString()
+	s2 := myint(num2).Sq()
+	c2 := myanotherint(num2).Cube()
+	fmt.Println(str2)
+	fmt.Println(s2)
+	fmt.Println(c2)
+
+	var num3 myanotherint = 30
 
 }
--- builtin.md
+++ builtin.md
@@ -1,7 +1,7 @@
 ## keywords
 
-break,case,const,continue,default,else,fallthrough,for,func,goto,if,import,package,range,return,switch,var,(17 of 25)
+break,case,const,continue,default,else,fallthrough,for,func,goto,if,import,map,package,range,return,struct,switch,type,var,(20 of 25)
 
 ## builtin
 
-append,cap,complex,copy,len,make,min,max,new,print,println (11 of 18)
+append,cap,complex,copy,delete,len,make,min,max,new,print,println, (12 of 18)
